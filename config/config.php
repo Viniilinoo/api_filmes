776879a5ae7af0238b9a9482c51d16779405fe06
unknown <viniciusino2105@gmail.com>
2025-05-20 20:32:52 -0300

    first commit

---
+++ config/config.php
@@ -0,0 +1,25 @@
+<?php
+
+    $host = 'localhost';
+    $usuario = 'root';
+    $senha = '';
+    $db = 'tmdb_filmes'; 
+
+    try{
+
+        $pdo = new PDO("mysql:host=$host; dbname=$db", $usuario, $senha);
+
+        $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+        // echo "Conexão realizada com sucesso!";
+
+    }
+
+    catch(PDOException $e){
+
+        echo "Não foi possível conectar!" . $e->getMessage();           
+
+
+    }
+
+?>
---
+++ view/filmes.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <title>Filmes Populares</title>
+    <link rel="stylesheet" type="text/css" href="style.css">
+</head>
+<body>
+ 
+    <h1>Filmes Populares</h1>
+    <hr>
+
+    <div>
+
+    </div>
+
+
+
+</body>
+</html>
