d588cedfee921721b2e3e7ae27b4211d9ed32955
unknown <viniciuslino2105@gmail.com>
2025-05-27 20:32:21 -0300

    updated

--- view/filmes.php
+++ view/filmes.php
@@ -10,7 +10,21 @@
     <h1>Filmes Populares</h1>
     <hr>
 
-    <div>
+    <div id="filmes-conteiner">
+
+        <?php
+
+            foreach ($filmes as $filmes) {
+
+                    echo "<div class='filme'>";
+                    echo "<h2>" . $filme['titulo'] . "</h2>";
+                    echo "<img src=" . $filme['poster'] . "alt='Filme'>";
+                    echo "<p>" . $filme['descricao'] . "</p>"; 
+                    echo "</div>";  
+
+            }
+
+        ?>
 
     </div>
 
